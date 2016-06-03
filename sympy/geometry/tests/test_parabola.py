340afa3465f5a3c43b7786945c6dc47f6ab7e3f4
Dimitra Konomi <t8130064@dias.aueb.gr>
2016-06-03 14:31:52 +0300

    New tests covering the cases of 4 different kinds of parabolas: vertical that opens up, vertical that opens down, horizontal that opens right and horizontal that opens left.

--- sympy/geometry/tests/test_parabola.py
+++ sympy/geometry/tests/test_parabola.py
@@ -13,32 +13,40 @@ def test_parabola_geom():
     p2 = Point(3, 7)
     p3 = Point(0, 4)
     p4 = Point(6, 0)
-    d1 = Line(Point(8,0), Point(8,9))
+    d1 = Line(Point(4,0), Point(4,9))
     d2 = Line(Point(7,6), Point(3,6))
-    d3 = Line(Point(3,5), Point(2,9))
+    d3 = Line(Point(4,0), slope = oo)
     d4 = Line(Point(7,6), slope = 0)
-    d5 = Line(Point(8,0), slope = oo)
-    d6 = Line(Point(0,7), slope = 1)
     
     pa1 = Parabola(None, d2)
     pa2 = Parabola(directrix = d1)
     pa3 = Parabola(p1, d1)
-    pa4 = Parabola(p2, d1)
-    pa5 = Parabola(p2, d5)
+    pa4 = Parabola(p2, d2)
+    pa5 = Parabola(p2, d4)
     pa6 = Parabola(p3, d2)
-    pa7 = Parabola(p3, d4)
-    pa8 = Parabola(p4, d5)
-    pa9 = Parabola(p1, d2)
+    pa7 = Parabola(p2, d1)
+    pa8 = Parabola(p4, d1)
+    pa9 = Parabola(p4, d3)
 
     # Basic Stuff
     assert pa1.focus == Point(0,0)
     assert pa2 == pa3
     assert pa4 == pa5
-    assert pa6 == pa7
-    assert pa8 != pa5
-    assert pa3 != pa9
-    assert pa6.focus == Point2D(0,4)
-    assert pa6.vertex == Point2D(0,5)
+    assert pa8 == pa9
+    assert pa4 != pa7
+    assert pa6 != pa7 
+    assert pa6.focus == Point2D(0, 4)
     assert pa6.focal_length == 1
+    assert pa6.p_parameter == -1
+    assert pa6.vertex == Point2D(0, 5)
     assert pa6.eccenticity == 1
-    
+    assert pa7.focus == Point2D(3, 7)
+    assert pa7.focal_length == 1/2
+    assert pa7.p_parameter == -1/2
+    assert pa7.vertex == Point2D(7/2, 7)
+    assert pa4.focal_length == 1/2
+    assert pa4.p_parameter == 1/2
+    assert pa4.vertex == Point2D(3, 13/2)
+    assert pa8.focal_length == 1
+    assert pa8.p_parameter == 1
+    assert pa8.vertex == Point2D(5, 0)
